085df7460d05b3e6319ed84cde9e8ea22653b92e
dragonkiller669 <a1800608@myy.haaga-helia.fi>
2020-05-28 22:55:06 +0300

    added notes

--- goprojekti2.go
+++ goprojekti2.go
@@ -12,10 +12,10 @@ func main() {
 	t3 := time.Date(2020,8,20,22,22,22,22,time.UTC)
 	t4 := time.Date(2020,8,19,22,22,22,22,time.UTC)
 	t5 := time.Date(2020,7,31,22,22,22,22,time.UTC)
-        tavarat := []struct {
-                nimi string
-                pvm time.Time
-        }{
+        tavarat := []struct { //couldn't get time.Date in an understandable form
+                nimi string //-rm, that the function reading the slice
+                pvm time.Time //would've understood before the time limit.
+        }{ //I need further research in software development on the time var
                 {"kalja", t5},
                 {"maito", t4},
 		{"kananmuna", t3},
@@ -28,11 +28,11 @@ func main() {
 
 sort.Slice(tavarat, func(i, j int) bool {
 	return tavarat[i].pvm.Before(tavarat[j].pvm) })
-
-        for _, tavara := range tavarat {
-                fmt.Println(tavara)
-
-	}
-}
+//sorts time.Time values from smallest to biggest
+        for _, tavara := range tavarat { //prints all structs in the slice
+                fmt.Println(tavara) //couldn't figure out a way to print them
+//				in my desired format starting with day of the mo
+	} // this was intended to be a last consumption day list of the products
+} //	in your fridge. (Viimeinen käyttöpäivä)
 
 
